befb33228a7e8b0850f347394cb323ac48dbb71b
vannesspeng <401041628@qq.com>
2017-11-28 14:05:46 +0800

    单例模式

---
+++ src/com/vanness/designmode/SingleTon/EnumSingleton.java
@@ -0,0 +1,9 @@
+package com.vanness.designmode.SingleTon;
+
+public enum EnumSingleton{
+    //单例对象
+    singleton;
+    public void hello(){
+        System.out.print("hello");
+    }
+}
---
+++ src/com/vanness/designmode/SingleTon/StaticInnerClassSingleTon.java
@@ -0,0 +1,15 @@
+package com.vanness.designmode.SingleTon;
+
+public class StaticInnerClassSingleTon {
+
+    private StaticInnerClassSingleTon(){}
+
+    public static StaticInnerClassSingleTon getSingleton(){
+        return SingletonHolder.SINGLETON;
+    }
+    private static class SingletonHolder{
+        private final static StaticInnerClassSingleTon SINGLETON =new StaticInnerClassSingleTon();
+    }
+}
+
+
