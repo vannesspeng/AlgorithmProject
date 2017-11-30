554e4edf801591608d37c9373814cec9bdb9a2db
vannesspeng <401041628@qq.com>
2017-11-30 19:14:57 +0800

    抽象工厂模式

---
+++ src/com/vanness/designmode/FactoryMode/AbstractFactory/HaierWashMachine.java
@@ -0,0 +1,7 @@
+package com.vanness.designmode.FactoryMode.AbstractFactory;
+
+public class HaierWashMachine implements ProductWashMachine{
+    public HaierWashMachine() {
+        System.out.println("生产了一台海尔洗衣机");
+    }
+}
